bc8fb78f4a03ea8ca19a0b32a044e27cd770551b
Icetune <109099573+Icetune@users.noreply.github.com>
2023-03-10 06:20:40 +0100

    working on food background

--- src/app/foods-page/foods-page.component.ts
+++ src/app/foods-page/foods-page.component.ts
@@ -7,4 +7,49 @@ import { Component } from '@angular/core';
 })
 export class FoodsPageComponent {
 
+  categorieList10: any = [];
+
+  constructor() {
+    this.x();
+  }
+
+  async x() {
+    await fetch('https://free-food-menus-api-production.up.railway.app/pizzas')
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error('Network response was not ok.');
+      })
+      .then(data => {
+        for (let i = 0; i < 10; i++) {
+          const menu = data[i];
+          this.categorieList10.push(menu);          
+        }
+        console.log('Eine Liste an ausgewähltem Essen (10x):', this.categorieList10);
+      })
+      .catch(error => {
+        console.log('There was a problem with the fetch operation:', error);
+      });
+  }
 }
+
+
+
+  // SOME OTHER FOODS
+  //
+  // /bbqs
+  // /best-foods
+  // /breads
+  // /burgers
+  // /chocolates
+  // /desserts
+  // /drinks
+  // /fried-chicken
+  // /ice-cream
+  // /pizzas
+  // /porks
+  // /sandwiches
+  // /sausages
+  // /steaks
+
