7b940c13f0d9ba2d11b25d014ab5bfba1ffe2a65
Icetune <109099573+Icetune@users.noreply.github.com>
2023-03-10 13:05:20 +0100

    need to improve search function

--- src/app/foods-page/foods-page.component.ts
+++ src/app/foods-page/foods-page.component.ts
@@ -7,50 +7,40 @@ import { Component } from '@angular/core';
 })
 export class FoodsPageComponent {
 
-  menuJSON: any = [
-    
-    {
-      'food': 'best-foods',
-      'menus': []
-    },
-    {
-      'food': 'pizzas',
-      'menus': []
-    },
-    {
-      'food': 'burgers',
-      'menus': []
-    },
-    {
-      'food': 'bbqs',
-      'menus': []
-    },
-    {
-      'food': 'drinks',
-      'menus': []
-    }
-  ];
-
+  foodWeServe = ['best-foods', 'pizzas', 'burgers', 'bbqs', 'drinks'];
+  menuJSON: any = [];
   upperCaseCategories = [];
   apiLoaded: boolean = false;
-
+  userSearchesFood: boolean = false;
 
 
   constructor() {
+    this.fillMenuJSON();
     this.upperCaseCategorieNames();
-    this.y();
+    this.loadApiIntoJSON();
   }
+
+
+  fillMenuJSON() {
+    this.foodWeServe.forEach(foodCategorie => {
+      this.menuJSON.push({
+        'food': foodCategorie,
+        'menus': [],
+      })
+    })
+  }
+
 
   upperCaseCategorieNames() {
     this.menuJSON.forEach(menu => {
       this.upperCaseCategories.push(menu.food.charAt(0).toUpperCase() + menu.food.slice(1));
-    });    
+    });
   }
 
-  async y() {
+
+  async loadApiIntoJSON() {
     for (let i = 0; i < this.menuJSON.length; i++) {
       const menuCategorie = this.menuJSON[i].food;
-
       await fetch(`https://free-food-menus-api-production.up.railway.app/${menuCategorie}`)
         .then(response => {
           if (response.ok) {
@@ -58,27 +48,58 @@ export class FoodsPageComponent {
           }
           throw new Error('Network response was not ok.');
         })
-
         .then(data => {
           for (let d = 0; d < 5; d++) {
             const menu = data[d];
+            menu['display'] = true;
             this.menuJSON[i].menus.push(menu);
           }
         })
-
         .catch(error => {
           console.log('There was a problem with the fetch operation:', error);
         });
     }
     console.log(this.menuJSON);
     this.apiLoaded = true;
-
   }
 
 
 
 
 
+  searchFood(search: string) {
+    if (search == '') {
+      this.userSearchesFood = false;
+      this.displayAllMenus();
+    } else {
+      this.userSearchesFood = true;
+      this.displaySearchedMenus(search);
+    }
+  }
+
+
+  displayAllMenus() {
+    this.menuJSON.forEach(categorie => {
+      categorie.menus.forEach(menu => {
+        menu.display = true;
+      });
+    });
+  }
+
+
+  displaySearchedMenus(search: string) {
+    this.menuJSON.forEach(categorie => {
+      categorie.menus.forEach(menu => {
+        let menuName = menu.name.toLowerCase();
+        let searchedName = search.toLowerCase();
+
+        if (!menuName.includes(searchedName)) {
+          menu.display = false;
+        }
+
+      });
+    });
+  }
 
 
 
