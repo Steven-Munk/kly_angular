079e72cb1ef6b8df258ed041913add634382f7ea
Icetune <109099573+Icetune@users.noreply.github.com>
2023-03-12 04:59:54 +0100

    foods page done

--- src/app/foods-page/foods-page.component.ts
+++ src/app/foods-page/foods-page.component.ts
@@ -13,6 +13,18 @@ export class FoodsPageComponent {
   apiLoaded: boolean = false;
   userSearchesFood: boolean = false;
 
+  foodCardOpen = false;
+  foodCardInfo = [
+    {
+      'food': '',
+      'dsc': '',
+      'amount': 1,
+      'price': 0,
+      'total': 0,
+      'img': ''
+    }
+  ];
+
 
   constructor() {
     this.fillMenuJSON();
@@ -41,26 +53,35 @@ export class FoodsPageComponent {
   async loadApiIntoJSON() {
     for (let i = 0; i < this.menuJSON.length; i++) {
       const menuCategorie = this.menuJSON[i].food;
-      await fetch(`https://free-food-menus-api-production.up.railway.app/${menuCategorie}`)
-        .then(response => {
-          if (response.ok) {
-            return response.json();
-          }
-          throw new Error('Network response was not ok.');
-        })
-        .then(data => {
-          for (let d = 0; d < 5; d++) {
-            const menu = data[d];
-            menu['display'] = true;
-            this.menuJSON[i].menus.push(menu);
-          }
-        })
-        .catch(error => {
-          console.log('There was a problem with the fetch operation:', error);
-        });
+      this.fetchApi(menuCategorie, i);
     }
-    console.log(this.menuJSON);
     this.apiLoaded = true;
+  }
+
+
+  async fetchApi(menuCategorie, i) {
+    await fetch(`https://free-food-menus-api-production.up.railway.app/${menuCategorie}`)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error('Network response was not ok.');
+      })
+      .then(data => {
+        this.pushIntoJSON(data, i);
+      })
+      .catch(error => {
+        console.log('There was a problem with the fetch operation:', error);
+      });
+  }
+
+
+  pushIntoJSON(data, i) {
+    for (let d = 0; d < 5; d++) {
+      const menu = data[d];
+      menu['display'] = true;
+      this.menuJSON[i].menus.push(menu);
+    }
   }
 
 
@@ -97,47 +118,6 @@ export class FoodsPageComponent {
       });
     });
   }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  foodCardOpen = false;
-  foodCardInfo = [
-    {
-      'food': '',
-      'dsc': '',
-      'amount': 1,
-      'price': 0,
-      'total': 0,
-      'img': ''
-    }
-  ];
 
 
   toggleFoodCard(id) {
