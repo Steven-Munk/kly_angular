bbe7c27ea9708662186050ff1514a4c3d1fdd4f9
Icetune <109099573+Icetune@users.noreply.github.com>
2023-03-14 14:17:07 +0100

    api only gets downloaded once now

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,68 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'kly_angular';
+
+  foodWeServe = ['best-foods', 'pizzas', 'burgers', 'bbqs', 'drinks'];
+  menuJSON: any = [];
+  upperCaseCategories = [];
+  apiLoaded: boolean = false;
+
+
+  constructor() {
+    this.fillMenuJSON();
+    this.upperCaseCategorieNames();
+    this.loadApiIntoJSON();
+  }
+
+
+  fillMenuJSON() {
+    this.foodWeServe.forEach(foodCategorie => {
+      this.menuJSON.push({
+        'food': foodCategorie,
+        'menus': [],
+      })
+    })
+  }
+
+
+  upperCaseCategorieNames() {
+    this.menuJSON.forEach(menu => {
+      this.upperCaseCategories.push(menu.food.charAt(0).toUpperCase() + menu.food.slice(1));
+    });
+  }
+
+
+  async loadApiIntoJSON() {
+    for (let i = 0; i < this.menuJSON.length; i++) {
+      const menuCategorie = this.menuJSON[i].food;
+      this.fetchApi(menuCategorie, i);
+    }
+    this.apiLoaded = true;
+  }
+
+
+  async fetchApi(menuCategorie, i) {
+    await fetch(`https://free-food-menus-api-production.up.railway.app/${menuCategorie}`)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error('Network response was not ok.');
+      })
+      .then(data => {
+        this.pushIntoJSON(data, i);
+      })
+      .catch(error => {
+        console.log('There was a problem with the fetch operation:', error);
+      });
+  }
+
+
+  pushIntoJSON(data, i) {
+    for (let d = 0; d < 5; d++) {
+      const menu = data[d];
+      menu['display'] = true;
+      this.menuJSON[i].menus.push(menu);
+    }
+  }
 }
--- src/app/foods-page/foods-page.component.ts
+++ src/app/foods-page/foods-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AppComponent } from '../app.component';
 
 @Component({
   selector: 'app-foods-page',
@@ -6,14 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./foods-page.component.scss']
 })
 
-export class FoodsPageComponent {
+export class FoodsPageComponent extends AppComponent {
 
-  foodWeServe = ['best-foods', 'pizzas', 'burgers', 'bbqs', 'drinks'];
-  menuJSON: any = [];
-  upperCaseCategories = [];
-  apiLoaded: boolean = false;
   userSearchesFood: boolean = false;
-
   foodCardOpen = false;
   foodCardInfo = [
     {
@@ -28,63 +24,8 @@ export class FoodsPageComponent {
   ];
 
 
-
   constructor() {
-    this.fillMenuJSON();
-    this.upperCaseCategorieNames();
-    this.loadApiIntoJSON();
-  }
-
-
-  fillMenuJSON() {
-    this.foodWeServe.forEach(foodCategorie => {
-      this.menuJSON.push({
-        'food': foodCategorie,
-        'menus': [],
-      })
-    })
-  }
-
-
-  upperCaseCategorieNames() {
-    this.menuJSON.forEach(menu => {
-      this.upperCaseCategories.push(menu.food.charAt(0).toUpperCase() + menu.food.slice(1));
-    });
-  }
-
-
-  async loadApiIntoJSON() {
-    for (let i = 0; i < this.menuJSON.length; i++) {
-      const menuCategorie = this.menuJSON[i].food;
-      this.fetchApi(menuCategorie, i);
-    }
-    this.apiLoaded = true;
-  }
-
-
-  async fetchApi(menuCategorie, i) {
-    await fetch(`https://free-food-menus-api-production.up.railway.app/${menuCategorie}`)
-      .then(response => {
-        if (response.ok) {
-          return response.json();
-        }
-        throw new Error('Network response was not ok.');
-      })
-      .then(data => {
-        this.pushIntoJSON(data, i);
-      })
-      .catch(error => {
-        console.log('There was a problem with the fetch operation:', error);
-      });
-  }
-
-
-  pushIntoJSON(data, i) {
-    for (let d = 0; d < 5; d++) {
-      const menu = data[d];
-      menu['display'] = true;
-      this.menuJSON[i].menus.push(menu);
-    }
+    super();   
   }
 
 
